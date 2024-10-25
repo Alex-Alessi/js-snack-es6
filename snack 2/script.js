f6ef3c1ba19d74b8b85f9a6ed81ffbe5b271c610
Alex-Alessi <alex.alessi246@gmail.com>
2024-10-25 15:57:30 +0200

    snack 2 complete

--- snack 2/script.js
+++ snack 2/script.js
@@ -4,3 +4,47 @@
 //* Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
 //* Generare numeri random al posto degli 0 nelle proprietà "punti" fatti e "falli subiti".
 //* Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const serieA = [
+  (squadra1 = {
+    nome: "Inter",
+    puntiFatti: 0,
+    falliSubiti: 0,
+  }),
+  (squadra2 = {
+    nome: "Juventus",
+    puntiFatti: 0,
+    falliSubiti: 0,
+  }),
+  (squadra3 = {
+    nome: "Milan",
+    puntiFatti: 0,
+    falliSubiti: 0,
+  }),
+  (squadra4 = {
+    nome: "Napoli",
+    puntiFatti: 0,
+    falliSubiti: 0,
+  }),
+];
+
+for (let i = 0; i < serieA.length; i++) {
+  serieA[i].puntiFatti = parseInt(Math.random() * 114) + 1;
+  serieA[i].falliSubiti = parseInt(Math.random() * 500) + 1;
+}
+
+const creaSquadra = (nome, falli) => {
+  const name = nome;
+  const falliSubiti = falli;
+  return { name, falliSubiti };
+};
+
+const creaLista = () => {
+  const ArrayFalliSubiti = [];
+  for (let i = 0; i < serieA.length; i++) {
+    ArrayFalliSubiti.push(creaSquadra(serieA[i].nome, serieA[i].falliSubiti));
+  }
+  return ArrayFalliSubiti;
+};
+
+console.log(creaLista());
