8798a8e4673a9c96d05cc8faca5f92d132f69b3b
Alex-Alessi <alex.alessi246@gmail.com>
2024-10-26 14:49:20 +0200

    snack 3 complete

--- snack 3/script.js
+++ snack 3/script.js
@@ -1,3 +1,17 @@
 //* Snack 3 (Bonus)
 //* Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 //* La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+const funz = (array, a, b) => {
+  const numIntermedi = [];
+  for (let i = 0; i < array.length; i++) {
+    if (a < i && b > i) {
+      numIntermedi.push(array[i]);
+    }
+  }
+  return numIntermedi;
+};
+
+const numeri = [2, 1, 6, 5, 8, 5, 7];
+const risultato = funz(numeri, 0, 6);
+console.log(risultato);
